fb019dc0155686aed28cc80aa7cc062664e1f143
Gabriel Porter <gporter71@gmail.com>
2022-10-02 23:51:56 -0400

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+import git
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('home.html')
+
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        repo = git.Repo('.', search_parent_directories=True)
+        origin = repo.remotes.origin
+        origin.pull()
+    else:
+        return 'Wrong event type', 400
+
+    return 'Updated PythonAnywhere successfully', 200
